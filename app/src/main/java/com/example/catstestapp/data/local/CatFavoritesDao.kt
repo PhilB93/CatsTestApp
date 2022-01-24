230804bbc91d0dbce78a5942b499b6e086e113ce
PhilB93 <F19931993b>
2022-01-24 13:42:51 +0300

    feat: Refactor Dao

--- app/src/main/java/com/example/catstestapp/data/local/CatFavoritesDao.kt
+++ app/src/main/java/com/example/catstestapp/data/local/CatFavoritesDao.kt
@@ -1,18 +1,16 @@
 package com.example.catstestapp.data.local
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.catstestapp.data.local.entity.CatEntity
 
 @Dao
 interface CatFavoritesDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCatEntity(catEntity: CatEntity)
 
-    @Query("DELETE FROM catentity  WHERE id IN(:id)")
-    suspend fun deleteCatEntity(id: String)
+    @Delete
+    suspend fun delete(catEntity: CatEntity)
 
     @Query("SELECT * FROM catentity")
     suspend fun getCatEntity(): List<CatEntity>
