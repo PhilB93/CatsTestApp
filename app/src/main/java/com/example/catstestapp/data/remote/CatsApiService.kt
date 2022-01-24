027979403b6ea68c62c704403daee685c0f94acf
PhilB93 <F19931993b>
2022-01-24 13:42:59 +0300

    feat: Refactor CatsApiService.kt

--- app/src/main/java/com/example/catstestapp/data/remote/CatsApiService.kt
+++ app/src/main/java/com/example/catstestapp/data/remote/CatsApiService.kt
@@ -1,16 +1,26 @@
 package com.example.catstestapp.data.remote
 
+import androidx.annotation.IntRange
 import com.example.catstestapp.BuildConfig.API_KEY
-import com.example.catstestapp.data.remote.model.CatDto
+import com.example.catstestapp.domain.model.Cat
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
+
+private const val VALUE_LIMIT = 10
 
 interface CatsApiService {
     @GET("breeds")
     @Headers("x-api-key: $API_KEY")
     suspend fun getCatItems(
-        @Query("limit") limit: Int,
-        @Query("page") page: Int
-    ): List<CatDto>
+        @Query("page")
+        @IntRange(from = 1)
+        page: Int,
+        @Query("limit")
+        @IntRange(
+            from = 1,
+            to = VALUE_LIMIT.toLong()
+        )
+        limit: Int
+    ): List<Cat>
 }
